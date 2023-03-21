0fdd3e6c6ffcc3d3060b293a8f060cdea7bfac01
serhiishf <unison447@gmail.com>
2023-03-21 15:17:28 +0200

    feat: Add global scss files includes normalize.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/index.scss';
 import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
