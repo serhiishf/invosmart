12f6ef71e7bd75143f30ad6e513c28aaacac6bed
serhiishf <unison447@gmail.com>
2024-02-22 15:56:12 +0200

    feat: Add options container to Select

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -21,6 +21,8 @@ function Select(props: SelectProps) {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const currentPlaceholder = !inputValue && !selectedValue ? placeholder : '';
+
   useEffect(() => {
     const wrapper = wrapperRef.current;
     const handleFocusIn = () => {
@@ -29,6 +31,7 @@ function Select(props: SelectProps) {
     const handleFocusOut = (event: FocusEvent) => {
       if (wrapper && !wrapper.contains(event.relatedTarget as Node)) {
         setIsFocused(false);
+        setInputValue('');
       }
     };
 
@@ -57,50 +60,55 @@ function Select(props: SelectProps) {
 
   return (
     <div className={styles.select} ref={wrapperRef}>
-      <FieldWrapper isFocused={isFocused} isHoverable={!isFocused} label={label}>
-        <div
-          className={classNames(
-            styles.select__control,
-            isFocused && styles['select__control--isFocused']
-          )}
-          onClick={handleControlClick}
-        >
-          <div className={styles.select__valueContainer}>
-            <div className={styles.select__valueItem}></div>
-            {!inputValue && placeholder && (
-              <div className={styles.select__placeholder}>{placeholder}</div>
+      <div className={styles.select__controlContainer}>
+        <FieldWrapper isFocused={isFocused} isHoverable={!isFocused} label={label}>
+          <div
+            className={classNames(
+              styles.select__control,
+              isFocused && styles['select__control--isFocused']
             )}
-            <div className={styles.select__inputContainer}>
-              <InputBase
-                autoFocus={isFocused}
-                paddingLeft="none"
-                paddingRight="none"
-                onChange={handleInputChange}
-                value={inputValue}
-                ref={inputRef}
-                readOnly={!isSearchable}
-              />
-            </div>
-          </div>
-          <div className={styles.select__suffixContainer}>
-            {isClearable && (
-              <div className={styles.select__buttonContainer}>
-                {(inputValue || selectedValue) && (
-                  <div className={styles.select__clearButtonWrap}>
-                    <IconButton tooltipMessage="TRANSLATE Clear" onClick={handleClearButtonClick}>
-                      <CloseIcon className={styles.select__closeIcon} />
-                    </IconButton>
-                  </div>
-                )}
+            onClick={handleControlClick}
+          >
+            <div className={styles.select__valueContainer}>
+              <div className={styles.select__valueItem}></div>
+              <div className={styles.select__inputContainer}>
+                <InputBase
+                  autoFocus={isFocused}
+                  paddingLeft="none"
+                  paddingRight="none"
+                  onChange={handleInputChange}
+                  value={inputValue}
+                  ref={inputRef}
+                  readOnly={!isSearchable}
+                  placeholder={currentPlaceholder}
+                />
               </div>
-            )}
-            <div className={styles.select__divider}></div>
-            <div className={styles.select__iconContainer}>
-              <ArrowIcon className={styles.select__arrowIcon} />
+            </div>
+            <div className={styles.select__suffixContainer}>
+              {isClearable && (
+                <div className={styles.select__buttonContainer}>
+                  {(inputValue || selectedValue) && (
+                    <div className={styles.select__clearButtonWrap}>
+                      <IconButton tooltipMessage="TRANSLATE Clear" onClick={handleClearButtonClick}>
+                        <CloseIcon className={styles.select__closeIcon} />
+                      </IconButton>
+                    </div>
+                  )}
+                </div>
+              )}
+              <div className={styles.select__divider}></div>
+              <div className={styles.select__iconContainer}>
+                <ArrowIcon className={styles.select__arrowIcon} />
+              </div>
             </div>
           </div>
+        </FieldWrapper>
+      </div>
+      {isFocused && (
+        <div className={classNames(styles.select__options, styles.options)}>
+          <div className={styles.options__placeholder}>No options</div>
         </div>
-      </FieldWrapper>
+      )}
     </div>
   );
 }
