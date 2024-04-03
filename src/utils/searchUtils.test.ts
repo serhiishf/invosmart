dba3fd239ae2efcf57d49b89109a9885d5f64b78
serhiishf <unison447@gmail.com>
2024-04-03 16:18:52 +0300

    feat: covered with tests filterOptions util, refactor another tests in searcUtils

--- src/utils/searchUtils.test.ts
+++ src/utils/searchUtils.test.ts
@@ -4,7 +4,9 @@ import {
   escapeForRegExp,
   createSearchRegExp,
   findMatchByIncreasingDepth,
+  filterOptions,
 } from './searchUtils';
+import { OptionType } from 'types/common';
 
 describe('escapeForRegExp', () => {
   it('should escape special RegExp characters', () => {
@@ -48,7 +50,7 @@ describe('escapeForRegExp', () => {
 });
 
 describe('createSearchRegExp', () => {
-  describe('matchStrategy: MatchStrategy.AnyMatch', () => {
+  describe('MatchStrategy.AnyMatch behavior', () => {
     it('should match everything with an empty search phrase', () => {
       const searchRegExp = createSearchRegExp('', MatchStrategy.AnyMatch);
       expect('Any string should match').toMatch(searchRegExp);
@@ -66,11 +68,10 @@ describe('createSearchRegExp', () => {
       const phrase = 'Hello.*+?^${}()|[]\\World';
       const searchRegExp = createSearchRegExp(phrase, MatchStrategy.AnyMatch);
       expect('Hello.*+?^${}()|[]\\World').toMatch(searchRegExp);
-      expect('Some random text ${}()|[]\\.*+?^').not.toMatch(searchRegExp);
     });
   });
 
-  describe('matchStrategy: MatchStrategy.StartString', () => {
+  describe('Behavior with MatchStrategy.StartString', () => {
     it('should match everything with an empty search phrase', () => {
       const searchRegExp = createSearchRegExp('', MatchStrategy.StartString);
       expect('Any string should match').toMatch(searchRegExp);
@@ -82,7 +83,6 @@ describe('createSearchRegExp', () => {
       const searchRegExp = createSearchRegExp(phrase, MatchStrategy.StartString);
       expect('lorem ipsum').toMatch(searchRegExp);
       expect('LOreMSomeText').toMatch(searchRegExp);
-      expect('Some random text').not.toMatch(searchRegExp);
     });
 
     it('should correct handle RegExp characters', () => {
@@ -96,22 +96,20 @@ describe('createSearchRegExp', () => {
       const phrase = 'start';
       const searchRegExp = createSearchRegExp(phrase, MatchStrategy.StartString);
       expect('start here').toMatch(searchRegExp);
-      expect('Please start here').not.toMatch(searchRegExp);
     });
   });
 
-  describe('matchStrategy: MatchStrategy.StartWord', () => {
+  describe('Behavior with MatchStrategy.StartWord', () => {
     it('should match any word with an empty search phrase', () => {
       const searchRegExp = createSearchRegExp('', MatchStrategy.StartWord);
       expect('Any word should match').toMatch(searchRegExp);
-      expect('').not.toMatch(searchRegExp);
+      expect('another phrase').toMatch(searchRegExp);
     });
 
     it('should be case insensitive', () => {
       const phrase = 'Lorem';
       const searchRegExp = createSearchRegExp(phrase, MatchStrategy.StartWord);
       expect('impsum LOREM').toMatch(searchRegExp);
-      expect('Some random text').not.toMatch(searchRegExp);
     });
 
     it('should correct handle RegExp characters', () => {
@@ -125,19 +123,53 @@ describe('createSearchRegExp', () => {
       const phrase = 'start';
       const searchRegExp = createSearchRegExp(phrase, MatchStrategy.StartWord);
       expect('Match Start here').toMatch(searchRegExp);
-      expect('Please notStart here').not.toMatch(searchRegExp);
     });
   });
 });
 
 describe('findMatchByIncreasingDepth', () => {
+  describe('Empty array behavior across matching depth strategies', () => {
+    const matchingDepthStrategies = [
+      MatchStrategy.AnyMatch,
+      MatchStrategy.StartWord,
+      MatchStrategy.StartString,
+    ];
+
+    test.each(matchingDepthStrategies)('should return index "-1" for maxDepth: %s', (maxDepth) => {
+      expect(findMatchByIncreasingDepth('some text', [], maxDepth)).toEqual(-1);
+    });
+  });
+
+  describe('Precedence of "StartString" matches in varying match strategies', () => {
+    const matchingDepthStrategies = [
+      MatchStrategy.AnyMatch,
+      MatchStrategy.StartWord,
+      MatchStrategy.StartString,
+    ];
+    const testStringsArray = [
+      '',
+      'not match string',
+      'match for anyPhrase',
+      'match for word phrase',
+      'phrase match start string',
+      'phrase next match start string',
+    ];
+
+    test.each(matchingDepthStrategies)(
+      '%s strategy should correctly prioritize "StartString" matches by returning index 4',
+      (maxDepth) => {
+        expect(findMatchByIncreasingDepth('phrase', testStringsArray, maxDepth)).toEqual(4);
+      }
+    );
+  });
+
   const stringsArray = [
     'Excom OU',
     'Alex Daimler',
     'testree',
     'flower computer',
     'second tree tree',
-    'nbH',
+    '',
     'dog markable',
     'yNl',
     'dave@simple.com',
@@ -159,82 +191,178 @@ describe('findMatchByIncreasingDepth', () => {
     'some email startWord@email.com',
   ];
 
-  describe('with empty array', () => {
-    const matchTypes = [MatchStrategy.AnyMatch, MatchStrategy.StartWord, MatchStrategy.StartString];
-
-    test.each(matchTypes)(
-      'should return "-1" for maxDepth: %s with an empty array',
-      (matchType) => {
-        expect(findMatchByIncreasingDepth('some text', [], matchType)).toEqual(-1);
-      }
-    );
-  });
-
-  describe('preference for starting string matches', () => {
-    const testCases = [MatchStrategy.AnyMatch, MatchStrategy.StartWord, MatchStrategy.StartString];
-
-    test.each(testCases)(
-      'should prioritize match at the start of the string for maxDepth: %s',
-      (maxDepth) => {
-        expect(findMatchByIncreasingDepth('tree', stringsArray, maxDepth)).toEqual(12);
-      }
-    );
-  });
-
-  describe('Max depth: Any match', () => {
+  describe('Behavior with MatchStrategy.AnyMatch', () => {
     const maxDepth = MatchStrategy.AnyMatch;
 
-    it('should find first match: Start word', () => {
+    it('should find first match: Start word and return index', () => {
       expect(findMatchByIncreasingDepth('house', stringsArray, maxDepth)).toEqual(15);
     });
 
-    it('should find first match: Any', () => {
+    it('should find first match: Any and return index', () => {
       expect(findMatchByIncreasingDepth('xcom', stringsArray, maxDepth)).toEqual(0);
     });
 
-    it('should not find any match and return "-1"', () => {
+    it('should not find any match and return index "-1"', () => {
       expect(findMatchByIncreasingDepth('not match', stringsArray, maxDepth)).toEqual(-1);
     });
 
-    it('should find middle part email correctly', () => {
+    it('should find middle part email correctly and return index', () => {
       expect(findMatchByIncreasingDepth('ob@simple', stringsArray, maxDepth)).toEqual(20);
     });
 
-    it('should find middle part telephone number correctly', () => {
+    it('should find middle part telephone number correctly and return index', () => {
       expect(findMatchByIncreasingDepth('858', stringsArray, maxDepth)).toEqual(13);
     });
   });
 
-  describe('Max depth: Start word', () => {
+  describe('Behavior with MatchStrategy.StartWord', () => {
     const maxDepth = MatchStrategy.StartWord;
 
-    it('should find first match: Start word', () => {
+    it('should find first match: Start word and return index', () => {
       expect(findMatchByIncreasingDepth('house', stringsArray, maxDepth)).toEqual(15);
     });
 
-    it('should return "-1" when no matches start with the query string or word', () => {
+    it('should return index "-1" when no matches start with the query string or word', () => {
       expect(findMatchByIncreasingDepth('xcom', stringsArray, maxDepth)).toEqual(-1);
     });
 
-    it('should find start email correctly', () => {
+    it('should find start email correctly and return index', () => {
       expect(findMatchByIncreasingDepth('startWord@e', stringsArray, maxDepth)).toEqual(24);
     });
 
-    it('should find a segment of a formatted telephone number', () => {
+    it('should find a segment of a formatted telephone number and return index', () => {
       expect(findMatchByIncreasingDepth('740', stringsArray, maxDepth)).toEqual(13);
       expect(findMatchByIncreasingDepth('18211', stringsArray, maxDepth)).toEqual(23);
     });
   });
 
-  describe('Max depth: Start String', () => {
+  describe('Behavior with MatchStrategy.StartString', () => {
     const maxDepth = MatchStrategy.StartString;
 
-    it('should return "-1" when no matches start with the query string', () => {
+    it('should return index "-1" when no matches start with the query string', () => {
       expect(findMatchByIncreasingDepth('house', stringsArray, maxDepth)).toEqual(-1);
     });
 
-    it('should find string starting from email', () => {
+    it('should find string starting from email and return index', () => {
       expect(findMatchByIncreasingDepth('dave@simple', stringsArray, maxDepth)).toEqual(8);
+    });
+  });
+});
+
+describe('filterOptions', () => {
+  const optionsArray: OptionType[] = [
+    { label: 'Fish Moon Cloud', value: 'Fish Moon Cloud' },
+    { label: 'Mountain Flower Bird', value: 'Mountain Flower Bird' },
+    { label: 'Star Cloud Stone', value: 'Star Cloud Stone' },
+    { label: 'Stone Mountain', value: 'Stone Mountain' },
+    { label: 'Sun Bird', value: 'Sun Bird' },
+    { label: 'Sky', value: 'Sky' },
+    { label: 'Cloud Cloud', value: 'Cloud Cloud' },
+    { label: 'Moon', value: 'Moon' },
+    { label: 'Water Cloud', value: 'Water Cloud' },
+    { label: 'Water Apple Sky', value: 'Water Apple Sky' },
+    { label: 'Bird', value: 'Bird' },
+    { label: 'Grass Mountain', value: 'Grass Mountain' },
+    { label: 'Sun Fish', value: 'Sun Fish' },
+    { label: 'Apple Apple', value: 'Apple Apple' },
+    { label: 'Apple Water', value: 'Apple Water' },
+  ];
+
+  const matchingStrategies = [
+    MatchStrategy.AnyMatch,
+    MatchStrategy.StartWord,
+    MatchStrategy.StartString,
+  ];
+
+  describe('Empty phrase behavior across match strategies', () => {
+    test.each(matchingStrategies)(
+      'should return unchanged array for matchStrategy: %s',
+      (matchStrategy) => {
+        expect(filterOptions('', optionsArray, matchStrategy)).toEqual(optionsArray);
+      }
+    );
+  });
+
+  describe('Empty optionsArray across match strategies', () => {
+    test.each(matchingStrategies)(
+      'should return empty array for matchStrategy: %s',
+      (matchStrategy) => {
+        expect(filterOptions('some random phrase', [], matchStrategy)).toEqual([]);
+      }
+    );
+  });
+
+  describe('Unmatching phrase across match strategies', () => {
+    test.each(matchingStrategies)(
+      'should return empty array for matchStrategy: %s',
+      (matchStrategy) => {
+        expect(filterOptions('some random phrase', optionsArray, matchStrategy)).toEqual([]);
+      }
+    );
+  });
+
+  describe('Case-insensitive filtering', () => {
+    const testOptionsArray: OptionType[] = [
+      { label: 'Bird magnetic', value: 'Bird magnetic' },
+      { label: 'bird', value: 'bird' },
+      { label: 'some random text', value: 'some random text' },
+      { label: 'BIRD lorem', value: 'BIRD lorem' },
+      { label: 'another random text', value: 'another random text' },
+    ];
+    const expectedFilteredOptionsArray: OptionType[] = [
+      { label: 'Bird magnetic', value: 'Bird magnetic' },
+      { label: 'bird', value: 'bird' },
+      { label: 'BIRD lorem', value: 'BIRD lorem' },
+    ];
+
+    test.each(matchingStrategies)(
+      'should be case-insensitive and return filtered array for matchStrategy: %s',
+      (matchStrategy) => {
+        expect(filterOptions('bird', testOptionsArray, matchStrategy)).toEqual(
+          expectedFilteredOptionsArray
+        );
+        expect(filterOptions('Bird', testOptionsArray, matchStrategy)).toEqual(
+          expectedFilteredOptionsArray
+        );
+        expect(filterOptions('BIRD', testOptionsArray, matchStrategy)).toEqual(
+          expectedFilteredOptionsArray
+        );
+      }
+    );
+  });
+
+  describe('Behavior with MatchStrategy.AnyMatch', () => {
+    const anyMatchStrategy = MatchStrategy.AnyMatch;
+
+    it('should return filtered array with all matches', () => {
+      expect(filterOptions('loud', optionsArray, anyMatchStrategy)).toEqual([
+        { label: 'Fish Moon Cloud', value: 'Fish Moon Cloud' },
+        { label: 'Star Cloud Stone', value: 'Star Cloud Stone' },
+        { label: 'Cloud Cloud', value: 'Cloud Cloud' },
+        { label: 'Water Cloud', value: 'Water Cloud' },
+      ]);
+    });
+  });
+
+  describe('Behavior with MatchStrategy.StartWord', () => {
+    const startWordStrategy = MatchStrategy.StartWord;
+
+    it('should return filtered array with all matches', () => {
+      expect(filterOptions('Bird', optionsArray, startWordStrategy)).toEqual([
+        { label: 'Mountain Flower Bird', value: 'Mountain Flower Bird' },
+        { label: 'Sun Bird', value: 'Sun Bird' },
+        { label: 'Bird', value: 'Bird' },
+      ]);
+    });
+  });
+
+  describe('Behavior with MatchStrategy.StartString', () => {
+    const startStringStrategy = MatchStrategy.StartString;
+
+    it('should return filtered array with all matches', () => {
+      expect(filterOptions('Sky', optionsArray, startStringStrategy)).toEqual([
+        { label: 'Sky', value: 'Sky' },
+      ]);
     });
   });
 });
