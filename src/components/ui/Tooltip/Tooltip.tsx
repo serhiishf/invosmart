fe0372ee0820d6e2c903204f7516c170f4962518
Serhii Shf <unison447@gmail.com>
2024-07-02 20:39:57 +0300

    feat: Add story for Tooltip, fix vulnerabilitie in package dependecies, small update tooltip

--- src/components/ui/Tooltip/Tooltip.tsx
+++ src/components/ui/Tooltip/Tooltip.tsx
@@ -2,19 +2,13 @@ import styles from './Tooltip.module.scss';
 import { forwardRef } from 'react';
 import { TooltipProps } from './types';
 import classNames from 'classnames';
-import IconArrow from '../../../assets/icons/arrowTriangle.svg?react';
 
 const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
-  ({ tooltipMessage, arrow, ...rest }: TooltipProps, ref) => {
+  ({ tooltipMessage, ...rest }: TooltipProps, ref) => {
     return (
       <div {...rest} ref={ref}>
         <div className={classNames(styles.tooltip)}>
           <div>{tooltipMessage}</div>
-          {arrow && (
-            <IconArrow
-              className={classNames(styles.tooltip__arrow, styles[`tooltip__arrow--${arrow}`])}
-            />
-          )}
         </div>
       </div>
     );
--- src/components/ui/Tooltip/types.ts
+++ src/components/ui/Tooltip/types.ts
@@ -2,5 +2,4 @@ import { HTMLAttributes } from 'react';
 
 export interface TooltipProps extends HTMLAttributes<HTMLDivElement> {
   tooltipMessage: string;
-  arrow?: 'left' | 'right' | 'topEnd' | 'topStart' | 'bottomEnd' | 'bottomStart' | 'top' | 'bottom';
 }
