027b8cf307624e4c59adc7a60222f7684aa0490b
Serhii Shf <unison447@gmail.com>
2024-08-14 21:48:05 +0300

    fix: Fixed problem with stories for IconButton component

--- src/components/ui/IconButton/IconButton.stories.tsx
+++ src/components/ui/IconButton/IconButton.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import IconButton from './IconButton';
-import * as icons from 'mocks/shared/icons';
+import * as Icons from 'mocks/shared/icons';
 import { CSSProperties } from 'react';
 import { IconButtonProps } from './types';
 
